669445aec5fbea1adf2dd850ac18144afcc41fce
Suyeon <msjgr2801@gmail.com>
2025-12-02 16:14:45 +0900

    feat(signup): 에러 메세지 관리 상수 추가

--- shared/types/zod.ts
+++ shared/types/zod.ts
@@ -1,6 +1,4 @@
 import { ZodIssueCode } from 'zod/v3';
 
-export type ExtendedZodErrorMessageMap = Record<
-  ZodIssueCode | 'default' | 'server',
-  string
->;
+export type ExtendedZodCode = ZodIssueCode | 'default' | 'server';
+export type ExtendedZodErrorMessageMap = Record<ExtendedZodCode, string>;
