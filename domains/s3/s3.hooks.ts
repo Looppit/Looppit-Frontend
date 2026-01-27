37d49dfaa6757c4c19a809d3bd1c4f2b954f1c91
Suyeon <msjgr2801@gmail.com>
2026-01-27 20:53:47 +0900

    feat(s3): s3 api 훅 타입 추가 - s3.hooks.ts: reqeust & response 타입 명시

--- domains/s3/s3.hooks.ts
+++ domains/s3/s3.hooks.ts
@@ -1,9 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { createPresignedUrl } from './s3.api';
+import { CreatePresignedUrlRequest, PresignedUrlResponse } from './s3.types';
 
 export const useCreatePresignedUrl = () => {
-  return useMutation({
+  return useMutation<PresignedUrlResponse, Error, CreatePresignedUrlRequest>({
     mutationFn: createPresignedUrl,
   });
 };
