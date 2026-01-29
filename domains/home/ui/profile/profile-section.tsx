59fd2698ba34beb561601cab8f1aad4eafc1dbfc
Suyeon <msjgr2801@gmail.com>
2026-01-29 18:24:13 +0900

    feat(user): 프로필 이미지 클릭 시 프로필 편집 페이지로 이동 - profile-section.tsx: 클릭 시 라우팅 이벤트 처리 로직 추가 - user-greeting.tsx: 클릭 이벤트 프로퍼티 추가

--- domains/home/ui/profile/profile-section.tsx
+++ domains/home/ui/profile/profile-section.tsx
@@ -1,14 +1,20 @@
+import { useRouter } from 'next/navigation';
+
 import { useGetUser } from '@/domains/user/hooks';
 
 import { UserGreeting } from './user-greeting';
 
 export function ProfileSection() {
+  const router = useRouter();
   const { data: user } = useGetUser();
 
   return (
-    <UserGreeting
-      name={user?.nickname ?? ''}
-      imgPath={user?.imagePath ?? null}
-    />
+    <div>
+      <UserGreeting
+        name={user?.nickname ?? ''}
+        imgPath={user?.imagePath ?? null}
+        onClick={() => router.push('/profile')}
+      />
+    </div>
   );
 }
