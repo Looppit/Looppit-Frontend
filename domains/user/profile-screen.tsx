6416cd46ca8003d8e4e0636338d1e4b86e83b4cd
Suyeon <msjgr2801@gmail.com>
2026-01-30 15:24:42 +0900

    feat(profile): 프로필 수정 api 연결 - profile-screen.tsx: 프로필 수정 api 연결

--- domains/user/profile-screen.tsx
+++ domains/user/profile-screen.tsx
@@ -1,11 +1,14 @@
+import { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
 
 import { Button } from '@/shared/ui/button';
 import { Form } from '@/shared/ui/form';
+import { Spacing } from '@/shared/ui/spacing';
 
-import { useGetUser } from './hooks';
+import { useGetUser, useUpdateProfile } from './hooks';
 import {
   EmailField,
   NicknameField,
@@ -17,6 +20,8 @@ import { UserProfileFormValues, userProfileFormSchema } from './user.types';
 
 export function ProfileScreen() {
   const { data: user } = useGetUser();
+  const { updateProfileMutations, isPending } = useUpdateProfile();
+
   const form = useForm<UserProfileFormValues>({
     resolver: zodResolver(userProfileFormSchema),
     values: {
@@ -26,6 +31,15 @@ export function ProfileScreen() {
     },
     mode: 'onChange',
   });
+
+  const handleUpdateProfile = useCallback(() => {
+    updateProfileMutations({
+      formValues: form.getValues(),
+      onSuccess: () => {
+        toast.success('프로필이 수정되었어요');
+      },
+    });
+  }, [updateProfileMutations, form]);
 
   return (
     <div className="flex flex-col h-full overflow-hidden relative">
@@ -38,9 +52,10 @@ export function ProfileScreen() {
             <NicknameField />
             <ContentField />
           </div>
-        </div>
-        <div className="absolute bottom-0 left-0 w-full p-6 pt-16 flex justify-center pointer-events-none">
-          <Button>저장하기</Button>
+          <Spacing size={108} />
+          <Button onClick={handleUpdateProfile} disabled={isPending}>
+            {isPending ? '저장 중...' : '저장하기'}
+          </Button>
         </div>
       </Form>
     </div>
