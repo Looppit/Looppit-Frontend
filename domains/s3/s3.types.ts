fb2ed9f40974be59c36eb707104c9762edcf91a5
Suyeon <msjgr2801@gmail.com>
2026-02-02 21:24:37 +0900

    feat(onboarding): s3 이미지 업로드 시 프로퍼티 추가 - s3.types.ts: `contentType` 프로퍼티 요청 시 추가 - s3.utils.ts: `contentType` 추출 유틸함수 - s3.type-guard.ts: `contentType` 타입 가드 함수 추가 - use-update-profile.tsx: API 전송시 변경된 프로퍼티 반영

--- domains/s3/s3.types.ts
+++ domains/s3/s3.types.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod';
 
+export const imageContentTypeSchema = z.enum(['JPG', 'PNG', 'JPEG']);
+
+export type ImageContentType = z.infer<typeof imageContentTypeSchema>;
+
 export const createPresignedUrlRequestSchema = z.object({
   fileName: z.string(),
+  contentType: imageContentTypeSchema,
 });
 
 export type CreatePresignedUrlRequest = z.infer<
--- domains/user/hooks/use-update-profile.ts
+++ domains/user/hooks/use-update-profile.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 
 import { useCreatePresignedUrl } from '@/domains/s3/s3.hooks';
+import { getImageContentType } from '@/domains/s3/s3.utils';
 
 import { useUpdateUser } from './use-user-query';
 import { UpdateUserRequest } from '../user.types';
@@ -20,19 +21,25 @@ export const useUpdateProfile = () => {
 
   const updateProfile = useCallback(
     async ({ form, onSuccess }: UpdateProfileOptions) => {
-      const { imgPath } = form;
-      const requestData: UpdateUserRequest = {
-        ...form,
-        imgPath: null,
-      };
-      if (imgPath instanceof File) {
-        const { url } = await createPresignedUrlMutation({
-          fileName: imgPath.name,
-        });
-        requestData.imgPath = url;
-      }
+      try {
+        const { imgPath } = form;
+        const requestData: UpdateUserRequest = {
+          ...form,
+          imgPath: null,
+        };
+        if (imgPath instanceof File) {
+          const contentType = getImageContentType(imgPath);
+          const { url } = await createPresignedUrlMutation({
+            fileName: imgPath.name,
+            contentType,
+          });
+          requestData.imgPath = url;
+        }
 
-      updateUserMutation(requestData, { onSuccess });
+        updateUserMutation(requestData, { onSuccess });
+      } catch (error) {
+        console.error(error);
+      }
     },
     [createPresignedUrlMutation, updateUserMutation],
   );
