2e40bb4b8de7c0dc95efe3d9b4ab8a553ae03c81
Suyeon <80307321+suyeon1218@users.noreply.github.com>
2026-02-20 15:47:05 +0900

    Revert "feat: og tag 설정 및 favicon 변경"

--- shared/constants/asset.ts
+++ shared/constants/asset.ts
@@ -1,4 +1,3 @@
 export const ASSET_URL = {
   DEFAULT_IMAGE: '/assets/default-image.png',
-  OPEN_GRAPH_IMAGE: '/assets/open-graph-image.png',
 };
--- shared/constants/environment.ts
+++ shared/constants/environment.ts
@@ -8,7 +8,6 @@ const envs = {
   urls: {
     apiEndPoint: process.env.NEXT_PUBLIC_API_BASE_URL,
     clientBaseUrl: process.env.NEXT_PUBLIC_API_CLIENT_BASE_URL,
-    domain: process.env.NEXT_PUBLIC_DOMAIN,
   },
   postHog: {
     postHogKey: process.env.NEXT_PUBLIC_POSTHOG_KEY,
